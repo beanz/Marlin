ac9f6315c3de31a83a224c40899c27ec12baeb0d
Mark Hindess <soft-github@temporalanomaly.com>
2014-02-04 19:22:47 +0000

    Dial gauge.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -449,7 +449,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 // For deltabots this means top and center of the Cartesian print volume.
 #define MANUAL_X_HOME_POS 0
 #define MANUAL_Y_HOME_POS 0
-#define MANUAL_Z_HOME_POS 556.5 // 573=dial gauge // 545  // For delta: Distance between nozzle and print surface after homing.
+#define MANUAL_Z_HOME_POS 573 // jh=556.5 // 573=dial gauge // 545  // For delta: Distance between nozzle and print surface after homing.
 
 //// MOVEMENT SETTINGS
 #define NUM_AXIS 4 // The axis order in all axis related arrays is X, Y, Z, E
